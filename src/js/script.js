0c78a7ae8e7805ef9d57cfeb31ddb2b0c8d0e434
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-08-05 17:57:12 +0300

    add DOMcontentLoaded

--- src/js/script.js
+++ src/js/script.js
@@ -1,95 +1,102 @@
-'use strict';
-const skills = document.querySelector('.skills__container'),
-      portfolio = document.querySelector('.portfolio__content'),
-      burger = document.querySelector('.header');
-const dataBase = {
-    //SKILS
-    svgies: [
-        "Java Script",
-        "HTML",
-        "CSS",
-        "C++",
-        "Figma",
-        "Git",
-        "GitHub",
-        "IELTS",
-        "Visual Studio",
-        "Visual Studio Code"
-    ],
-    stars: [
-        4,
-        4,
-        4,
-        2,
-        3,
-        4,
-        4,
-        3,
-        4,
-        4
-    ],
-    //PORTFOLIO
-    screens: [
-        "Coca Cola",
-        "Titans of cnc",
-        "Design blog"
-    ],
-    ScreensUrl: [
-        "https://www.coca-cola.ru/",
-        "https://academy.titansofcnc.com/",
-        "https://www.invisionapp.com/inside-design"
-    ]
-};
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    const skills = document.querySelector('.skills__container'),
+        portfolio = document.querySelector('.portfolio__content'),
+        burger = document.querySelector('.header');
+    const dataBase = {
+        //SKILS
+        svgies: [
+            "Java Script",
+            "HTML",
+            "CSS",
+            "C++",
+            "Figma",
+            "Git",
+            "GitHub",
+            "IELTS",
+            "Visual Studio",
+            "Visual Studio Code"
+        ],
+        stars: [
+            4,
+            4,
+            4,
+            2,
+            3,
+            4,
+            4,
+            3,
+            4,
+            4
+        ],
+        //PORTFOLIO
+        screens: [
+            "Coca Cola",
+            "Titans of cnc",
+            "Design blog"
+        ],
+        ScreensUrl: [
+            "https://www.coca-cola.ru/",
+            "https://academy.titansofcnc.com/",
+            "https://www.invisionapp.com/inside-design"
+        ]
+    };
 
-const addSkills = (svgies, stars, parent) => {
-    parent.innerHTML = '';
+    const addSkills = (svgies, stars, parent) => {
+        parent.innerHTML = '';
 
-    svgies.forEach((item, ir) => {
-        let starsblock = '';
-        let starslist = '';
-        if(item == ""){
-        starsblock += `
+        svgies.forEach((item, ir) => {
+            let starsblock = '';
+            let starslist = '';
+            if (item == "") {
+                starsblock += `
                 <div class="skills__logo"></div>
                 <div class="skills__name"></div>
-        `;}else{
-        starsblock += `
+        `;
+            } else {
+                starsblock += `
                 <div class="skills__logo">
                     <img src="src/img/icons/${item}.svg" alt="${item}" class="skills__svg">
                 </div>
                 <div class="skills__name">
                     <p>${item}</p>
                 </div>
-        `;}
-        for(let i = 0; i < 5 ; i++){
-            if(stars[ir] == ''){
-                starslist = false;
-                    break;
+        `;
             }
-            if(i < stars[ir]){
-                starslist += `
+            for (let i = 0; i < 5; i++) {
+                if (stars[ir] == '') {
+                    starslist = false;
+                    break;
+                }
+                if (i < stars[ir]) {
+                    starslist += `
                     <img src="src/img/StarBlack.svg" alt="star" class="skills__star">
                 `;
-            }else{
-                starslist += `
+                } else {
+                    starslist += `
                     <img src="src/img/Star.svg" alt="star" class="skills__star">
-            `;}
-        }
-        if(starslist == false){
-            starsblock += `<div class="skills__stars"></div>`;
-        }else{
-            starsblock += `<div class="skills__stars">
+            `;
+                }
+            }
+            if (starslist == false) {
+                starsblock += `<div class="skills__stars"></div>`;
+            } else {
+                starsblock += `<div class="skills__stars">
             ${starslist}
-        </div>`;}
-        parent.innerHTML += `<div class="skills__box">
+        </div>`;
+            }
+            parent.innerHTML += `<div class="skills__box">
         ${starsblock}
         </div>`;
-    });
-};
-const addPortfolio = (img, url, parent) => {
-    parent.innerHTML = '';
+        });
+    };
 
-    img.forEach((item,i) => {
-        parent.innerHTML += `<div class="portfolio__blok">
+    
+    const addPortfolio = (img, url, parent) => {
+        parent.innerHTML = '';
+
+        img.forEach((item, i) => {
+            parent.innerHTML += `<div class="portfolio__blok">
         
             <div class="portfolio__img">
                 <img src="src/img/Sites/${item}.jpg" alt="${item}">
@@ -99,128 +106,134 @@ const addPortfolio = (img, url, parent) => {
             </div>
 
         </div>`;
+        });
+    };
+
+    burger.addEventListener('click', (event) => {
+        burger.querySelector('.header__burger').classList.toggle('active');
+        burger.querySelector('.header__menu').classList.toggle('active');
+        document.querySelector('.home__language').classList.toggle('active');
+        document.querySelector('body').classList.toggle('lock');
     });
-};
-
-burger.addEventListener('click', (event) => {
-   burger.querySelector('.header__burger').classList.toggle('active');
-   burger.querySelector('.header__menu').classList.toggle('active');
-   document.querySelector('.home__language').classList.toggle('active');
-   document.querySelector('body').classList.toggle('lock');
-});
 
 
-addSkills(dataBase.svgies, dataBase.stars, skills);
-addPortfolio(dataBase.screens, dataBase.ScreensUrl,portfolio);
+    addSkills(dataBase.svgies, dataBase.stars, skills);
+    addPortfolio(dataBase.screens, dataBase.ScreensUrl, portfolio);
 
-/////modal///////
+    /////modal///////
 
-const btnSendMessage = document.querySelector('[data-modal]'),
-      modal = document.querySelector('.modal'),
-      send = document.querySelector('[data-send]'),
-      closeWindow = document.querySelector('[data-close]');
+    const btnSendMessage = document.querySelector('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        send = document.querySelector('[data-send]'),
+        closeWindow = document.querySelector('[data-close]');
 
-function openModal() {
-    btnSendMessage.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.toggle('show');
-    document.body.style.overflow = 'hidden';
-    if(window.screen.width > 1455){
-    document.body.style.margin = '0 16px 0 0';}
-});
-}
-openModal();
-function closeModal() {
-    modal.classList.toggle('show');
-    document.body.style.overflow = '';
-    document.body.style.margin = '';
-}
-
-modal.addEventListener('click', (e) => {
-    if (e.target === modal || e.target.getAttribute('data-close') == "") {
-        closeModal();
+    function openModal() {
+        btnSendMessage.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+            if (window.screen.width > 1455) {
+                document.body.style.margin = '0 16px 0 0';
+            }
+        });
     }
-});
-
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape' && modal.classList.contains('show')) {
-        closeModal();
+    openModal();
+    function closeModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+        document.body.style.margin = '';
     }
-});
 
-//Form
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == "") {
+            closeModal();
+        }
+    });
 
-const form = document.querySelector('[data-form]');
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 
-const message = {
-    loading: 'src/img/form/spinner.svg',
-    success: 'success',
-    failure: 'failure'
-};
+    //Form
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    const form = document.querySelector('[data-form]');
 
-    let statusMessage = document.createElement('img');
-    statusMessage.src = message.loading;
-    statusMessage.style.cssText = `
+    const message = {
+        loading: 'src/img/form/spinner.svg',
+        success: 'success',
+        failure: 'failure'
+    };
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.style.cssText = `
         display: block;
         margin: 0 auto;
     `;
-    send.style.display = 'none';
-    form.insertAdjacentElement('afterend', statusMessage);
+        send.style.display = 'none';
+        form.insertAdjacentElement('afterend', statusMessage);
 
-    const formData = new FormData(form);
+        const formData = new FormData(form);
 
-    const object = {};
-    formData.forEach(function(value, key){
-        object[key] = value;
-    });
+        const object = {};
+        formData.forEach(function (value, key) {
+            object[key] = value;
+        });
 
-    fetch('server.php', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-    })
-    .then(data => data.text())
-    .then(data => {
-        console.log(data);
-        showThanksModal(message.success);
-        form.reset();
-        statusMessage.remove();
-    }).catch(() => {
-        showThanksModal(message.failure);
-    }).finally(() => {
-        form.reset();
-        send.style.display = 'block';
-    });
+        fetch('server.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                form.reset();
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+                send.style.display = 'block';
+            });
 
-    function showThanksModal(message) {
-        const prevModalDialog = document.querySelector('.modal__dialog');
+        function showThanksModal(message) {
+            const prevModalDialog = document.querySelector('.modal__dialog');
 
-        prevModalDialog.classList.add('hide');
-        openModal();
+            prevModalDialog.classList.add('hide');
+            openModal();
 
-        const thanksModal = document.createElement('div');
-        thanksModal.classList.add('modal__dialog');
-        thanksModal.innerHTML = `
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `
             <div class="modal__content">
                 <div class="modal__close" data-close>×</div>
                 <div class="modal__title">${message}</div>
             </div>
         `;
-        document.querySelector('.modal').append(thanksModal);
-        setTimeout(() => {
-            thanksModal.remove();
-            prevModalDialog.classList.remove('hide');
-            openModal();
-            if(modal.classList.contains('show')){
-                closeModal();
-            }
-        }, 2000);
-    }
+            document.querySelector('.modal').append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                prevModalDialog.classList.remove('hide');
+                openModal();
+                if (modal.classList.contains('show')) {
+                    closeModal();
+                }
+            }, 2000);
+        }
+    });
+
+    fetch('http://localhost:3000/skills')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
+
 });
-
-
