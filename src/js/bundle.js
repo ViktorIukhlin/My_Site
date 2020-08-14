9a9bb87af3a08358e2a3f867eff46c2841ebcbb0
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-08-14 14:49:45 +0300

    add meta tegs

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -440,13 +440,19 @@ __webpack_require__.r(__webpack_exports__);
 function scroll() {
 
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset < 100) {
+        if (window.pageYOffset < 100 || window.screen.width < 1300) {
             document.querySelector('.scrollUp').classList.add('hide-slow');
         }else{
             document.querySelector('.scrollUp').classList.remove('hide-slow');
         }
-    });
 
+        if(window.screen.width < 1300 && window.pageYOffset > 650){
+            document.querySelector('.scrollUpForMobile').classList.remove('scrollUpForMobile_hide');
+        }else if(window.screen.width < 1300 && window.pageYOffset < 650){
+            document.querySelector('.scrollUpForMobile').classList.add('scrollUpForMobile_hide');
+        }
+    });
+    
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (scroll);
--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,13 +1,19 @@
 function scroll() {
 
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset < 100) {
+        if (window.pageYOffset < 100 || window.screen.width < 1300) {
             document.querySelector('.scrollUp').classList.add('hide-slow');
         }else{
             document.querySelector('.scrollUp').classList.remove('hide-slow');
         }
-    });
 
+        if(window.screen.width < 1300 && window.pageYOffset > 650){
+            document.querySelector('.scrollUpForMobile').classList.remove('scrollUpForMobile_hide');
+        }else if(window.screen.width < 1300 && window.pageYOffset < 650){
+            document.querySelector('.scrollUpForMobile').classList.add('scrollUpForMobile_hide');
+        }
+    });
+    
 }
 
 export default scroll;
