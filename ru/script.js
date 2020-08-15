974acb5a57cb89697bb08aa3f22034d51d8b10bb
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-08-15 10:03:34 +0300

    ++

--- ru/script.js
+++ ru/script.js
@@ -8,11 +8,11 @@ window.addEventListener('DOMContentLoaded', () => {
     skillsLogo.forEach(item => {
         item.src = `../src/img/icons/${item.alt}.svg`;
     });
-    skillsStars.forEach(item => {
-        item.src = `../src/img/${item.alt}.svg`;
-    });
     portfolioImg.forEach(item => {
         item.src = `../src/img/Sites/${item.alt}.jpg`;
+    });
+    skillsStars.forEach(item => {
+        item.src = `../src/img/${item.alt}.svg`;
     });
 
     setTimeout(() => {
